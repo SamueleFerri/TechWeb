a4a3cdc36a1e2f3593ec6c797cc5bf5fd2c72251
nicolastrada <nicolastrada5@gmail.com>
2024-12-10 16:47:47 +0100

    Add favicon for Website

--- DigitalPhoto/resources/views/welcome1.blade.php
+++ DigitalPhoto/resources/views/welcome1.blade.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="DigitalPhoto">
+    <link rel="icon" type="image/x-icon" href="faviconDP.ico">
     {{-- <meta name="keywords" content="Male_Fashion, unica, creative, html"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
