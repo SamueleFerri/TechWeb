120b6242fe7419c938faad1980f7e8333eb4538d
nicolastrada <nicolastrada5@gmail.com>
2024-12-16 17:57:32 +0100

    Some fixes

--- DigitalPhoto/resources/views/bag.blade.php
+++ DigitalPhoto/resources/views/bag.blade.php
@@ -36,11 +36,7 @@
 </head>
 
 <body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
+    
     <!-- Offcanvas Menu Begin -->
     <div class="offcanvas-menu-overlay"></div>
     <div class="offcanvas-menu-wrapper">
