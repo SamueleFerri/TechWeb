a50ffaecd9c0f0dbe090409004106deec804f3dd
nicolastrada <nicolastrada5@gmail.com>
2025-01-14 16:45:11 +0100

    Fix Profile Admi

--- DigitalPhoto/app/Http/Controllers/ProfileAdminController.php
+++ DigitalPhoto/app/Http/Controllers/ProfileAdminController.php
@@ -34,7 +34,7 @@ class ProfileAdminController extends Controller
 
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.edit_admin')->with('status', 'profile-updated');
     }
 
     /**
